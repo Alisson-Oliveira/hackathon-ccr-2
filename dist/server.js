e997ff1078a3e69aa7e26a6504130e27c5d621e1
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2021-01-17 12:13:43 -0300

    Update cors to typescript

--- dist/server.js
+++ dist/server.js
@@ -11,13 +11,15 @@ require("reflect-metadata");
 require("./database");
 const app = express_1.default();
 dotenv_1.default.config();
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    app.use(cors_1.default());
-    next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "");
+//   next();
+// });
+app.use(cors_1.default({
+    origin: "*",
+    methods: "GET, PUT, POST, DELETE"
+}));
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.listen(process.env.PORT || 3333);
--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,16 @@ const app = express();
 
 dotenv.config();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  app.use(cors());
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "");
+//   next();
+// });
+
+app.use(cors({
+  origin: "*",
+  methods: "GET, PUT, POST, DELETE"
+}));
 
 app.use(express.json());
 app.use(routes);
