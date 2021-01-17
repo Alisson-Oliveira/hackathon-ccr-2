5ff21755808e9f9dffa8b1615a8de089e6249f28
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2021-01-17 11:35:08 -0300

    Update cors.

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,13 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(routes);
 
