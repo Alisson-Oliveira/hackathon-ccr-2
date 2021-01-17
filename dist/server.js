5c49dc4c1f9c36ec099faffb8c49f6d2836d8fd4
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2021-01-17 11:58:53 -0300

    Update Methods.

--- dist/server.js
+++ dist/server.js
@@ -11,7 +11,13 @@ require("reflect-metadata");
 require("./database");
 const app = express_1.default();
 dotenv_1.default.config();
-app.use(cors_1.default());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    app.use(cors_1.default());
+    next();
+});
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.listen(process.env.PORT || 3333);
--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  app.use(cors());
   next();
 });
 
